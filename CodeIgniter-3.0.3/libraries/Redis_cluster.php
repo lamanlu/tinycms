7d83d248db20410ac370bf9e89f107211d89405e
lamanlu <bonacc@126.com>
2016-03-07 19:29:22 +0800

    xxx

---
+++ CodeIgniter-3.0.3/libraries/Redis_cluster.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package Redis Cluster Connecter
+ * @author LamanLu
+ * @since 2016-02-28
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Redis_cluster {
+    
+    protected $_Redis = null;
+    
+    public function __construct($configFile = array()) {
+        $CI =& get_instance();
+        
+        $configName = array_shift($configFile);
+        
+        if ($CI->config->load($configName, TRUE, TRUE)){
+            $config = $CI->config->item($configName);
+        }
+        
+        if(empty($config)){
+            exit('No redis MQ config file');
+        }
+        
+        $hostArr = array();
+        foreach($config as $conf){
+            $hostArr[] = $conf['host'].':'.$conf['port'];
+        }
+
+        $this->_Redis= new RedisCluster(NULL,$hostArr);        
+    }
+    
+    public function getInstance(){
+        return $this->_Redis;
+    }
+    
+    
+}
---
+++ user/application/config/lm_config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 项目配置参数，调用方式同config.php内参数相同
+ * @author LamanLu
+ * @since 2016-01-27
+ */
+
+//数据表前缀配置参数
+$config['dbprefix'] = 'pre_';
+
+//密码SALT
+$config['salt_key'] = '&N(D(J@s8291';
+
